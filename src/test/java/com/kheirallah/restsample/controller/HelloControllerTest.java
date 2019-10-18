cbb4ecfa21c788bce4b44ba90a5b2ad91d24049a
Kheirallah Ashkar <kheirallah.j.ashkar@tsgforce.com>
2019-10-17 19:32:44 -0500

    controller test added

---
+++ src/test/java/com/kheirallah/restsample/controller/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package com.kheirallah.restsample.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    public void setMockMvc(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void shouldReturnGreeting() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Greetings from Spring Boot!")));
+    }
+}
