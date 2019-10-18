9474543d396b457a791992fc19aceb0028c9f005
Kheirallah Ashkar <kheirallah.j.ashkar@tsgforce.com>
2019-10-17 19:16:17 -0500

    initial project with simple controller

---
+++ src/main/java/com/kheirallah/restsample/Main.java
@@ -0,0 +1,18 @@
+package com.kheirallah.restsample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ src/main/java/com/kheirallah/restsample/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.kheirallah.restsample.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Greetings from Spring Boot!";
+    }
+}
